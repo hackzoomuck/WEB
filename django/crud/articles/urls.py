bdf6dbba44f20ae021d7be8d3670ce89a00a53a1
leejieun94 <xaq03061@gmail.com>
2019-10-31 17:42:17 +0900

    191031 url&app name, RESTful 주소 정리

--- django/crud/articles/urls.py
+++ django/crud/articles/urls.py
@@ -1,13 +1,39 @@
 from django.urls import path
 from . import views
 
+#새로운 변수
+app_name = 'articles'
+
 urlpatterns = [
-    path('', views.index),
-    path('new/', views.new), #데이터를 입력받아서 다른 페이지로 넘기는 것
-    path('create/', views.create), 
+    path('', views.index, name = 'index'),
+    path('new/', views.new, name = 'new'), #데이터를 입력받아서 다른 페이지로 넘기는 것 # GET,POST /articles/new/
+    #path('create/', views.create, name = 'create'),  #RESTful하지는 않음(x) 
     #layout은 같은데 컨텐츠만 바뀌게 하는 것/ variable routing/ 게시글을 구분할 수 있는 colum ->id
-    path('<int:pk>/', views.detail),
-    path('<int:pk>/delete/', views.delete),
-    path('<int:pk>/edit/', views.edit),#수정하는 양식이 있는 페이지
-    path('<int:pk>/update/', views.update),
+    path('<int:pk>/', views.detail, name = 'detail'),
+    path('<int:pk>/edit/', views.edit, name = 'edit'),#수정하는 양식이 있는 페이지
+   # path('<int:pk>/update/', views.update, name = 'update'),
+    path('<int:pk>/delete/', views.delete, name = 'delete'),
 ]
+
+#URL Name -> 각각의 path
+#path('주소/', views.함수, name='이름' )
+# {% url 'url 이름' %} => /students/new/
+# [장점]
+# 1. 주소의 변경이 필요할 때, urls.py에서만 수정해주면 됨.
+# 2. 마지막 '/'를 빼먹는 실수를 차단할 수 있음.
+
+#APP Name - 특정 app의 urls.py 자체
+# {% url 'articles:index'%}
+# {% url 'app_name:path_name'%}
+
+#RESTful
+
+#Django는 PUT/PATCH/DELETE 불가능하기에
+#GET /articles/2/edit #=> 수정 페이지 보여줌
+#POST /articles/edit #=> 수정 작업 수행
+#웹프레임워크는 두 메소드를 다르게 받아드림. 구분.
+
+#ex)
+#GET /users/1 유저1번 가져옴
+#PUT /users/1 유저1번 수정
+#DELETE /users/1 유저1번 삭제
--- django/crud/articles/views.py
+++ django/crud/articles/views.py
@@ -10,24 +10,35 @@ def index(request):
     return render(request, 'articles/index.html',context)
 
 def new(request):
-    return render(request, 'articles/new.html')
-
-def create(request): #post 요청을 받음
-    #넘어온 데이터 받기
-    #이유2번 - POST(http body)라는 것을 통해 넘어옴
-    #title=request.GET.get('title')
-    #content=request.GET.get('content')
+    if  request.method == 'POST': #=> 'GET' 'POST' 담겨있음. string으로 담김.
+    # student를 생성함 (def create)
     title=request.POST.get('title')
     content=request.POST.get('content')
-
-    #새로운 글을 쓰는 부분, article에는 뭐가 들어갈까? 게시글인스턴스
-    # article.title
-    # article.content
     article = Article.objects.create(title=title, content=content)
+    article.save()
+    return redirect('articles:detail',article.pk)
 
-    #return render(request, 'articles/create.html',context)
-    #다른 곳으로 요청을 한다. 다른 곳으로 연결을 한다. get요청을 받는 페이지로 돌려 보내야함.
-    return redirect(f'/articles/{article.pk}/')
+    else :
+    #new page를 보여주면 됨.
+    return render(request, 'articles/new.html')
+
+# def create(request): #post 요청을 받음
+#     #넘어온 데이터 받기
+#     #이유2번 - POST(http body)라는 것을 통해 넘어옴
+#     #title=request.GET.get('title')
+#     #content=request.GET.get('content')
+#     title=request.POST.get('title')
+#     content=request.POST.get('content')
+
+#     #새로운 글을 쓰는 부분, article에는 뭐가 들어갈까? 게시글인스턴스
+#     # article.title
+#     # article.content
+#     article = Article.objects.create(title=title, content=content)
+
+#     #return render(request, 'articles/create.html',context)
+#     #다른 곳으로 요청을 한다. 다른 곳으로 연결을 한다. get요청을 받는 페이지로 돌려 보내야함.
+#     #return redirect(f'/articles/{article.pk}/')
+#     return redirect('articles:detail',article.pk)
 
 def detail(request, pk): #parameter 적어줘야함.
     #1개만 찾아 올 것임.
@@ -39,29 +50,44 @@ def detail(request, pk): #parameter 적어줘야함.
     return render(request,'articles/detail.html',context)
 
 def delete(request, pk):
-    article = Article.objects.get(pk=pk)
-    article.delete()
-    return redirect('/articles/')
+    if request.method =='POST':
+        article = Article.objects.get(pk=pk)
+        article.delete()
+        return redirect('articles:index')
 
 def edit(request, pk):
-    article = Article.objects.get(pk=pk)
-    context = {
+        #1. pk에 해당하는 article 가져오기
+        article = Article.objects.get(pk=pk)
+    if request.method=="POST":
+        #2. update로 데이터를 넘기는 것.
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+
+        #3. edit로부터 넘어온 데이터 가져오기
+        article.title=title
+        article.content=content
+        article.save()
+
+        return redirect(f'/articles/{article.pk}/') #detail페이지로 리다이렉트
+    else:
+        article = Article.objects.get(pk=pk)
+        context = {
         'article': article,
-    }
-    return render(request, '/articles/edit.html', context)
+        }
+        return render(request, '/articles/edit.html', context)
 
-def update(request, pk):
-    #1. pk에 해당하는 article 가져오기
-    article = Article.objects.get(pk=pk)
-    #2. update로 데이터를 넘기는 것.
-    title = request.POST.get('title')
-    content = request.POST.get('content')
+# def update(request, pk):
+#     #1. pk에 해당하는 article 가져오기
+#     article = Article.objects.get(pk=pk)
+#     #2. update로 데이터를 넘기는 것.
+#     title = request.POST.get('title')
+#     content = request.POST.get('content')
 
-    #3. edit로부터 넘어온 데이터 가져오기
-    article.title=title
-    article.content=content
-    article.save()
+#     #3. edit로부터 넘어온 데이터 가져오기
+#     article.title=title
+#     article.content=content
+#     article.save()
 
-    return redirect(f'/articles/{article.pk}/') #detail페이지로 리다이렉트
+#     return redirect(f'/articles/{article.pk}/') #detail페이지로 리다이렉트
 
      
