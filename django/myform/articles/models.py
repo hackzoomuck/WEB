6b983163087e8d933e6ac3d06adb41a24dc7ee55
leejieun94 <xaq03061@gmail.com>
2019-11-18 17:11:46 +0900

    191118 Django M:N relation

--- django/myform/articles/models.py
+++ django/myform/articles/models.py
@@ -10,4 +10,10 @@ class Article(models.Model):
     #1:n(USER:ARTICLES)
     #어떤 모델과 관계할것인지.
     user= models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-
+    # article을 작성한 user -> article.user
+    # user가 작성한 모든 article -> user.article_set.all()
+    # M:N [LIKE] - USER:ARTICLE
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_articles')
+    # article에 좋아요 누른 모든 user -> article.like_users.all()
+    # user가 좋아요 누른 모든 article ->user.article_set.all()
+    #-> user.like_article.all()
